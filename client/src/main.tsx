828500b3f4f4f3e2f4b6bf6784297fe7b34404cd
Acurasa <unxetra@gmail.com>
2025-03-01 18:28:30 +0100

    added redux & rtk query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/layout/style.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './app/routes/Routes.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./app/layout/style.css";
 
-createRoot(document.getElementById('root')!).render(
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/routes/Routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store/store"; 
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
